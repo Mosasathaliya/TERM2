875e6978227eba18eb9abf33b4e207b8d57dfd14
Hassan Muhammed mirza Mirza <mirzapallikunnu@gmail.com>
2025-07-17 05:14:54 +0000

    لعبة الكلمات المبعثرة   JUST OPEN POP UP THIS LINK   https://mumble-jumb

--- src/components/mumble-jumble-app.tsx
+++ src/components/mumble-jumble-app.tsx
@@ -6,7 +6,7 @@ import * as React from 'react';
 export function MumbleJumbleApp() {
   return (
     <iframe
-      src="/mumble-jumble/index.html"
+      src="https://mumble-jumble-108473853069.us-west1.run.app/"
       className="w-full h-full border-0"
       title="Mumble Jumble App"
     ></iframe>
